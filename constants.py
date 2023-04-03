9eaa72dcf8703edaaab99d47c5ca0d0065e6ca1a
Dillon Walton <dillonwalton10@gmail.com>
2023-04-03 11:04:36 -0400

    additional push

--- constants.py
+++ constants.py
@@ -11,3 +11,12 @@ class Contants:
 
     # Boltzmann's constant
     k = 1.38064852e-23
+
+    # Speed of light
+    c = 299792458
+
+    # Avogadro's number
+    N_A = 6.02214076e23
+
+    # Charge of an electron
+    e = 1.60217662e-19
